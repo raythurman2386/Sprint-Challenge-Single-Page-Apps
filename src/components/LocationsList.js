b33097f25de6015ea68317cb9d3e634a8d265f5f
raythurman2386 <raymondthurman5@gmail.com>
2019-09-15 19:32:04 -0400

    initial setup of the locations list

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import LocationCard from './LocationCard'
 
-export default function LocationsList() {}
+export default function LocationsList() {
+  // set up hook for location state
+  const [locations, setLocations] = useState([])
+
+  // set up the useEffect to grab the locations
+  useEffect(() => {
+    Axios
+      // Get the data
+      .get()
+      // Do something with the data
+      .then()
+      // Do something with the error
+      .catch()
+  }, [])
+
+  return (
+    // map over the locations and send to the locationCard component
+    <div>
+      {locations &&
+        locations.map((location, index) => (
+          <LocationCard key={index} location={location} />
+        ))}
+    </div>
+  )
+}
