1eccdf9af1675e278b914bad3e9bb01e6fc91653
raythurman2386 <raymondthurman5@gmail.com>
2019-09-15 20:26:59 -0400

    added react spring

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+import { useSpring, animated, config } from 'react-spring'
 
 // Components
 import CharacterCard from './CharacterCard'
@@ -7,6 +8,14 @@ import CharacterCard from './CharacterCard'
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([])
+
+  // Animation stretch
+  const cardAnimation = useSpring({
+    from: { opacity: 0 },
+    opacity: 1,
+    delay: 400,
+    config: config.gentle,
+  })
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -19,7 +28,11 @@ export default function CharacterList() {
   // console.log(characters)
 
   return (
-    <section className='character-list grid-view'>
+    <section
+      animated
+      className='character-list grid-view'
+      style={cardAnimation}
+    >
       {/* <h2>TODO: `array.map()` over your state here!</h2> */}
       {characters &&
         characters.map((character, index) => (
