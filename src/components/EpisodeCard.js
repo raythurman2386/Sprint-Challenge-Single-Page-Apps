f2c49bb7c8de732ff53c89a42bcc3ef69045955c
raythurman2386 <raymondthurman5@gmail.com>
2019-09-15 20:15:42 -0400

    MVP Completed

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
 
-const EpisodeCard = () => {
-  return <div></div>
+export default function EpisodeCard({ episode }) {
+  // console.log(episode)
+
+  return (
+    <Card>
+      <Card.Content header={episode.name} />
+      <Card.Content description={episode.air_date} />
+    </Card>
+  )
 }
-
-export default EpisodeCard
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
-const EpisodeList = () => {
-  return <div></div>
+// Components
+import EpisodeCard from './EpisodeCard'
+
+export default function EpisodeList() {
+  // set up hook for location state
+  const [episodes, setEpisodes] = useState([])
+
+  // set up the useEffect to grab the locations
+  useEffect(() => {
+    Axios
+      // Get the data
+      .get('https://rickandmortyapi.com/api/episode/')
+      // Do something with the data
+      .then(res => setEpisodes(res.data.results))
+      // Do something with the error
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    // map over the locations and send to the locationCard component
+    <section className='character-list grid-view'>
+      {episodes &&
+        episodes.map(episode => (
+          <EpisodeCard key={episode.id} episode={episode} />
+        ))}
+    </section>
+  )
 }
-
-export default EpisodeList
