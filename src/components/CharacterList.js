946f7efc2d3167f6238fde43faebc51274cf6420
raythurman2386 <raymondthurman5@gmail.com>
2019-09-15 21:12:19 -0400

    Very basic pagination in place

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { useSpring, animated, config } from 'react-spring'
 import styled from 'styled-components'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Button } from 'semantic-ui-react'
 
 // Character List Component
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([])
+  // trying to set up a pagination
+  const [page, setPage] = useState(1)
 
   // Animation stretch
   const cardAnimation = useSpring({
@@ -20,21 +22,38 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    Axios.get('https://rickandmortyapi.com/api/character/')
+    Axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then(res => setCharacters(res.data.results))
       .catch(err => console.log(err))
-  }, [])
+  }, [page])
 
   // console.log(characters)
+  const nextPage = () => {
+    setPage(page + 1)
+  }
+
+  const prevPage = () => {
+    setPage(page - 1)
+  }
 
   return (
-    <StyledSection className='character-list grid-view' style={cardAnimation}>
-      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
-      {characters &&
-        characters.map((character, index) => (
-          <CharacterCard key={index} character={character} />
-        ))}
-    </StyledSection>
+    <>
+      {page === 1 ? (
+        <Button onClick={nextPage}>Next Page</Button>
+      ) : (
+        <>
+          <Button onClick={prevPage}>Prev Page</Button>{' '}
+          <Button onClick={nextPage}>Next Page</Button>
+        </>
+      )}
+      <StyledSection className='character-list grid-view' style={cardAnimation}>
+        {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+        {characters &&
+          characters.map((character, index) => (
+            <CharacterCard key={index} character={character} />
+          ))}
+      </StyledSection>
+    </>
   )
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,7 +5,9 @@ export default function SearchForm({ onSearch }) {
   // STRETCH TODO: Add stateful logic for query/form data
   const [name, setName] = useState('')
 
-  const handleInputChange = () => {}
+  const handleInputChange = e => {
+    setName(e.target.value)
+  }
 
   return (
     <section className='search-form'>
